c379a20dc9a5be1d1a193344158d79b17149d144
Roy <137373309+RoyilEs@users.noreply.github.com>
2024-12-03 16:43:31 +0800

    VideoDtoFetch课程章节卡片资源 (#5)

--- api/xuexitong/XueXiTongCardApi.go
+++ api/xuexitong/XueXiTongCardApi.go
@@ -63,7 +63,7 @@ func (e *APIError) Error() string {
 	return e.Message
 }
 
-func (cache *XueXiTUserCache) VideoDtoFetch(p *entity.PointVideoDto) (bool, error) {
+func (cache *XueXiTUserCache) VideoDtoFetch(p *entity.PointVideoDto) (string, error) {
 	params := url.Values{}
 	params.Set("k", strconv.Itoa(p.FID))
 	params.Set("flag", "normal")
@@ -73,45 +73,41 @@ func (cache *XueXiTUserCache) VideoDtoFetch(p *entity.PointVideoDto) (bool, erro
 	resp, err := http.NewRequest(method, fmt.Sprintf("%s/%s?%s", APIChapterCardResource, p.ObjectID, params.Encode()), nil)
 	// resp, err := p.Session.Client.Get(fmt.Sprintf("%s/%s?%s", APIChapterCardResource, p.ObjectID, params.Encode()))
 	if err != nil {
-		return false, err
+		return "", err
 	}
-
+	resp.Header.Add("Host", " mooc1-api.chaoxing.com")
+	resp.Header.Add("Connection", " keep-alive")
+	resp.Header.Add("User-Agent", " Mozilla/5.0 (Linux; Android 12; SM-N9006 Build/V417IR; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/95.0.4638.74 Mobile Safari/537.36 (schild:e9b05c3f9fb49fef2f516e86ac3c4ff1) (device:SM-N9006) Language/zh_CN com.chaoxing.mobile/ChaoXingStudy_3_6.3.7_android_phone_10822_249 (@Kalimdor)_4627cad9c4b6415cba5dc6cac39e6c96")
+	resp.Header.Add("X-Requested-With", " XMLHttpRequest")
+	resp.Header.Add("Accept", " */*")
+	resp.Header.Add("Sec-Fetch-Site", " same-origin")
+	resp.Header.Add("Sec-Fetch-Mode", " cors")
+	resp.Header.Add("Sec-Fetch-Dest", " empty")
+	resp.Header.Add("Referer", " https://mooc1-api.chaoxing.com/ananas/modules/video/index_wap.html?v=372024-1121-1947")
+	resp.Header.Add("Accept-Language", " zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7")
 	resp.Header.Add("Cookie", cache.cookie)
 
 	res, err := client.Do(resp)
 	if err != nil {
-		return false, err
+		return "", err
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != http.StatusOK {
-		return false, fmt.Errorf("failed to fetch video, status code: %d", res.StatusCode)
+		return "", fmt.Errorf("failed to fetch video, status code: %d", res.StatusCode)
 	}
+	body, err := ioutil.ReadAll(res.Body)
 
-	var jsonResponse map[string]interface{}
-	if err := json.NewDecoder(resp.Body).Decode(&jsonResponse); err != nil {
-		return false, err
-	}
-
-	p.DToken = jsonResponse["dtoken"].(string)
-	p.Duration = int(jsonResponse["duration"].(float64))
-	p.Title = jsonResponse["filename"].(string)
-
-	if jsonResponse["status"].(string) == "success" {
-		p.Logger.Printf("Fetch successful: %s", p)
-		return true, nil
-	}
-
-	p.Logger.Println("Fetch failed")
-	return false, nil
+	return string(body), nil
 }
 
-func VideoDtoPlayReport(p *entity.PointVideoDto, playingTime int) (map[string]interface{}, error) {
+func (cache *XueXiTUserCache) VideoDtoPlayReport(p *entity.PointVideoDto, playingTime int) (map[string]interface{}, error) {
 	clipTime := fmt.Sprintf("0_%d", p.Duration)
 	hash := md5.Sum([]byte(fmt.Sprintf("[%s][%s][%s][%d][%s][%d][%s]",
 		p.PUID, p.JobID, p.ObjectID, playingTime*1000, "d_yHJ!$pdA~5", p.Duration*1000, clipTime)))
 	enc := hex.EncodeToString(hash[:])
 
+	client := &http.Client{}
 	params := url.Values{}
 	params.Set("otherInfo", p.OtherInfo)
 	params.Set("playingTime", strconv.Itoa(playingTime))
@@ -127,16 +123,22 @@ func VideoDtoPlayReport(p *entity.PointVideoDto, playingTime int) (map[string]in
 	params.Set("dtype", "Video")
 	params.Set("view", "pc")
 	params.Set("_t", strconv.FormatInt(time.Now().UnixNano()/1e6, 10))
+	method := "GET"
 
-	reqURL := fmt.Sprintf("%s/%d/%s?%s", APIVideoPlayReport, p.FID, p.DToken, params.Encode())
-	resp, err := p.Session.Client.Get(reqURL)
+	resp, err := http.NewRequest(method, fmt.Sprintf("%s/%s?%s", APIChapterCardResource, p.ObjectID, params.Encode()), nil)
+	//reqURL := fmt.Sprintf("%s/%d/%s?%s", APIVideoPlayReport, p.FID, p.DToken, params.Encode())
+	//resp, err := p.Session.Client.Get(reqURL)
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
+	res, err := client.Do(resp)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("failed to report play, status code: %d", resp.StatusCode)
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to fetch video, status code: %d", res.StatusCode)
 	}
 
 	var jsonResponse map[string]interface{}
