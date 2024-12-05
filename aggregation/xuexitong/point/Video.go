37161f9e44aa2f22a6e6c1376cf4349444bd1eaf
Roy <qq3392313023@163.com>
2024-12-05 22:45:33 +0800

    update: 58s限制

--- aggregation/xuexitong/point/Video.go
+++ aggregation/xuexitong/point/Video.go
@@ -14,14 +14,19 @@ func ExecuteVideo(cache *api.XueXiTUserCache, p *entity.PointVideoDto) {
 	if state, _ := action.VideoDtoFetchAction(cache, p); state {
 		log.Printf("开始模拟播放....%d:%d开始\n", p.PlayTime, p.Duration)
 		var playingTime = p.PlayTime
+		var flag = 0
 		for {
-			playReport, _ := cache.VideoDtoPlayReport(p, playingTime)
-			if gojsonq.New().JSONString(playReport).Find("isPassed").(bool) == true {
-				log.Println("播放结束")
-				playingTime = p.Duration
-				break
+			if flag == 58 {
+				playReport, _ := cache.VideoDtoPlayReport(p, playingTime)
+				playingTime += flag
+				flag = 0
+				if gojsonq.New().JSONString(playReport).Find("isPassed").(bool) == true {
+					log.Println("播放结束")
+					playingTime = p.Duration
+					break
+				}
 			}
-			playingTime += 1
+			flag += 1
 			log.Printf("播放中....%d:%d\n", playingTime, p.Duration)
 			time.Sleep(time.Second * 1)
 		}
