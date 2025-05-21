5fe587675e6e8524ea152268bc75e33cf0c5b2b3
Changbaiqi <changbaiqi@foxmail.com>
2025-05-22 05:30:05 +0800

    update: 更新学习通答题测试

--- aggregation/xuexitong/XutXiTongWorkAction.go
+++ aggregation/xuexitong/XutXiTongWorkAction.go
@@ -1,16 +1,20 @@
 package xuexitong
 
 import (
-	"fmt"
 	"github.com/yatori-dev/yatori-go-core/api/entity"
 	"github.com/yatori-dev/yatori-go-core/api/xuexitong"
 )
 
 // WorkNewSubmitAnswerAction 提交答题
-func WorkNewSubmitAnswerAction(userCache *xuexitong.XueXiTUserCache, question entity.Question) {
+func WorkNewSubmitAnswerAction(userCache *xuexitong.XueXiTUserCache, question entity.Question, isSubmit bool) string {
+	submitState := ""
+	if isSubmit {
+		submitState = "1"
+	}
 	answer, _ := userCache.WorkNewSubmitAnswer(question.CourseId, question.ClassId, question.Knowledgeid, question.Cpi,
 		question.JobId, question.TotalQuestionNum, question.AnswerId, question.WorkAnswerId, question.Api,
 		question.FullScore, question.OldSchoolId, question.OldWorkId, question.WorkRelationId, question.Enc_work,
-		question, "")
-	fmt.Println(answer)
+		question, submitState)
+	//fmt.Println(answer)
+	return answer
 }
